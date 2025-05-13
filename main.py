7da4b09f5de281de234a599d4f595edd2fb56ff2
Nguyen Vu An <an.nguyen@wabtec.com>
2025-05-13 10:28:26 +0700

    feat(main.py): Random note on one string generator

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import random
+
 def main():
-    print("Hello from internal-music!")
+    # Unversal Variable
+    diatonic_Note = ["C","D","E","F","G","A","B"]
+    while True:
+        single_note = random.randint(0,7)
+        print(f"Note to play on single string: {diatonic_Note[single_note]}")
+        # ask if you still want to play
+        print("continue (Default Yes)? (Y/N)")
+        ans = input()
+        if ans.upper() != "N":
+            continue
+        else:
+            break
+
 
 
 if __name__ == "__main__":
