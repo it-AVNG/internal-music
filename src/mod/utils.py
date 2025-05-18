2889f9337d0a52d8a80f8bd37cdb121b37b7e8f4
banzdec <it.avng@gmail.com>
2025-05-18 11:57:16 +0700

    chore: add log to utils modules

--- src/mod/utils.py
+++ src/mod/utils.py
@@ -3,14 +3,18 @@ Create a random play
 + In which each string will be randomly generated.
 + In which each notes will be randomly picked from a list.
 """
-
+import logging
 import random
 
+module_logger = logging.getLogger(__name__)
 
 def randomizeStringNotes(
     string_list: list[str], note_list: list[str]
 ) -> tuple[str, str]:
+    logger = logging.getLogger(f"{module_logger.name}.randomizeStringNotes")
+    logger.info("select a random string from pool")
     gstring = random.choice(string_list)
+    logger.info("select a random tone from pool")
     notes = random.choice(note_list)
-
+    logger.info("return selected combination")
     return gstring, notes
